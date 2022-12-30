183db6596eb3d871a5362d3226400d740bf6409d
Your Name Main <you@example.com>
2022-12-30 04:34:01 -0800

    Tour Plan added

--- Components/Gallery.tsx
+++ Components/Gallery.tsx
@@ -5,16 +5,16 @@ const Gallery = () => {
         <div>
             <section className="py-6 bg-light dark:text-gray-50">
 	<div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4">
-		<img src="https://source.unsplash.com/random/301x301/" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 dark:bg-gray-500 aspect-square" />
+		<img src="https://gotravel.qodeinteractive.com/wp-content/uploads/2016/11/single-gallery-5-image-1.jpg" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 dark:bg-gray-500 aspect-square" />
 		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://gotravel.qodeinteractive.com/wp-content/uploads/2016/10/single-gallery-1-image-6.jpg" />
 		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://gotravel.qodeinteractive.com/wp-content/uploads/2016/10/single-gallery-1-image-1.jpg" />
 		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://gotravel.qodeinteractive.com/wp-content/uploads/2016/10/single-gallery-1-image-7.jpg" />
 		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://gotravel.qodeinteractive.com/wp-content/uploads/2016/10/single-gallery-1-image-5.jpg" />
-		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?4" />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://gotravel.qodeinteractive.com/wp-content/uploads/2016/10/single-gallery-1-image-3.jpg" />
 		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://gotravel.qodeinteractive.com/wp-content/uploads/2016/10/single-gallery-1-image-4.jpg" />
-		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?6" />
-		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?7" />
-		<img src="https://source.unsplash.com/random/302x302/" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-1 md:row-start-3 dark:bg-gray-500 aspect-square" />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://gotravel.qodeinteractive.com/wp-content/uploads/2016/11/single-gallery-5-image-3.jpg" />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square" src="https://gotravel.qodeinteractive.com/wp-content/uploads/2016/11/single-gallery-5-image-3.jpg" />
+		<img src="https://gotravel.qodeinteractive.com/wp-content/uploads/2016/10/single-gallery-1-image-7.jpg" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-1 md:row-start-3 dark:bg-gray-500 aspect-square" />
 	</div>
 </section>
         </div>
