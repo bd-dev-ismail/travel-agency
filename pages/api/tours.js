3a18885a2533e7fb969b379a6ba7521db3223426
Md. Shakibur Rahman <shakibur.cmt@gmail.com>
2023-01-01 01:10:49 +0600

    api

---
+++ pages/api/tours.js
@@ -0,0 +1,116 @@
+[
+    {
+      "image": "https://i.ibb.co/VYwrYRD/solvenia.jpg",
+      "thumbline": [
+        "https://i.ibb.co/z6SrbDc/istockphoto-1145064928-170667a.jpg",
+        "https://i.ibb.co/z6wQ7Zm/istockphoto-938498086-170667a.jpg",
+        "https://i.ibb.co/FqfN6RQ/istockphoto-543480238-170667a.jpg",
+        "https://i.ibb.co/vXjvMX0/istockphoto-1192667979-170667a.jpg",
+        "https://i.ibb.co/v1KWwSr/istockphoto-119919494-170667a.jpg",
+        "https://i.ibb.co/mCLgrnK/istockphoto-494314515-170667a.jpg",
+        "https://i.ibb.co/km8ctHK/istockphoto-825151702-170667a.jpg",
+        "https://i.ibb.co/q1ypSmw/istockphoto-1365455496-170667a.jpg",
+        "https://i.ibb.co/0yrkfpd/istockphoto-930309134-170667a.jpg",
+        "https://i.ibb.co/mq4kqkk/istockphoto-629376126-170667a.jpg"
+      ],
+      "price": "56",
+      "day": "12",
+      "name": "East Europe",
+      "subName": "Solvenia, Hungary, czech"
+    },
+    {
+      "image": "https://i.ibb.co/3ytb08s/scotLand.jpg",
+      "thumbline": [
+        "https://i.ibb.co/G7Ygf8N/railway-2439189-340.jpg",
+        "https://i.ibb.co/PjXRcBc/lake-192990-340.jpg",
+        "https://i.ibb.co/10ZV0RJ/lake-192991-340.webp",
+        "https://i.ibb.co/2cVwLSj/cattle-192976-340.webp",
+        "https://i.ibb.co/DLZYq6j/bridge-192982-340.jpg",
+        "https://i.ibb.co/D5PTmgn/stone-circles-1853340-340.jpg",
+        "https://i.ibb.co/km8ctHK/istockphoto-825151702-170667a.jpg",
+        "https://i.ibb.co/q1ypSmw/istockphoto-1365455496-170667a.jpg",
+        "https://i.ibb.co/0yrkfpd/istockphoto-930309134-170667a.jpg",
+        "https://i.ibb.co/mq4kqkk/istockphoto-629376126-170667a.jpg"
+      ],
+      "price": "952",
+      "day": "14",
+      "name": "UK Trip",
+      "subName": "England, Scotland, Wales"
+    },
+    
+    {
+      "image": "https://i.ibb.co/qNM6jKv/italy.jpg",
+      "thumbline": [
+        "https://i.ibb.co/8rpSyGy/village-279013-340.jpg",
+        "https://i.ibb.co/bBpcdtY/cathedral-1066314-340.jpg",
+        "https://i.ibb.co/59DsZmN/sunset-2080072-340.webp",
+        "https://i.ibb.co/xMmqjvm/lago-di-limides-3025780-340.webp",
+        "https://i.ibb.co/WBvNX3y/street-3453557-340.jpg",
+        "https://i.ibb.co/km8ctHK/istockphoto-825151702-170667a.jpg",
+        "https://i.ibb.co/q1ypSmw/istockphoto-1365455496-170667a.jpg",
+        "https://i.ibb.co/0yrkfpd/istockphoto-930309134-170667a.jpg",
+        "https://i.ibb.co/mq4kqkk/istockphoto-629376126-170667a.jpg"
+      ],
+      "price": "300",
+      "day": "11",
+      "name": "Grand Italy",
+      "subName": "Italy"
+    },
+    {
+      "image": "https://i.ibb.co/VMNmPPs/img3.jpg",
+      "thumbline": [
+        "https://i.ibb.co/9gYQTxm/schilthorn-3033448-340.jpg",
+        "https://i.ibb.co/74nLfKH/mountains-1516733-340.jpg",
+        "https://i.ibb.co/D5PTmgn/stone-circles-1853340-340.jpg",
+        "https://i.ibb.co/km8ctHK/istockphoto-825151702-170667a.jpg",
+        "https://i.ibb.co/8NTHK6z/winter-4697776-340.jpg",
+        "https://i.ibb.co/m0381WD/tree-1959267-340.webp",
+        "https://i.ibb.co/q1ypSmw/istockphoto-1365455496-170667a.jpg",
+        "https://i.ibb.co/s5sW5b7/mountains-1547302-340.jpg",
+        "https://i.ibb.co/0yrkfpd/istockphoto-930309134-170667a.jpg",
+        "https://i.ibb.co/mq4kqkk/istockphoto-629376126-170667a.jpg"
+      ],
+      "price": "400",
+      "day": "13",
+      "name": "Swiss Alps Trip",
+      "subName": "Austria, Switzerland"
+    },
+    {
+      "image": "https://i.ibb.co/6t8bXg2/img2.jpg",
+      "thumbline": [
+        "https://i.ibb.co/MnBLY43/fountain-461552-340.jpg",
+        "https://i.ibb.co/D5PTmgn/stone-circles-1853340-340.jpg",
+        "https://i.ibb.co/km8ctHK/istockphoto-825151702-170667a.jpg",
+        "https://i.ibb.co/q1ypSmw/istockphoto-1365455496-170667a.jpg",
+        "https://i.ibb.co/1XGkNB9/valencia-1049389-340.jpg",
+        "https://i.ibb.co/YjT3sVK/sunset-5536777-340.jpg",
+        "https://i.ibb.co/kMH4FKt/cathedral-427997-340.jpg",
+        "https://i.ibb.co/0yrkfpd/istockphoto-930309134-170667a.jpg",
+        "https://i.ibb.co/PhQCtWh/water-1283963-340.webp",
+        "https://i.ibb.co/mq4kqkk/istockphoto-629376126-170667a.jpg"
+      ],
+      "price": "305",
+      "day": "9",
+      "name": "Grand Spain Madrid",
+      "subName": "Spain"
+    },
+    {
+      "image": "https://i.ibb.co/N1tyHPg/img1.jpg",
+      "thumbline": [
+        "https://i.ibb.co/CvxXys1/boats-2758962-340.jpg",
+        "https://i.ibb.co/C0gLxqM/hot-air-balloons-5630493-340.jpg",
+        "https://i.ibb.co/D5PTmgn/stone-circles-1853340-340.jpg",
+        "https://i.ibb.co/km8ctHK/istockphoto-825151702-170667a.jpg",
+        "https://i.ibb.co/q1ypSmw/istockphoto-1365455496-170667a.jpg",
+        "https://i.ibb.co/hDQwLcV/istanbul-915076-340.jpg",
+        "https://i.ibb.co/cJtB6B3/river-4873198-340.jpg",
+        "https://i.ibb.co/0yrkfpd/istockphoto-930309134-170667a.jpg",
+        "https://i.ibb.co/ZNfwJm8/hot-air-balloons-1773468-340.jpg",
+        "https://i.ibb.co/mq4kqkk/istockphoto-629376126-170667a.jpg"
+      ],
+      "price": "205",
+      "day": "8",
+      "name": "Grand Turkey",
+      "subName": "Turkey"
+    }
+  ]
